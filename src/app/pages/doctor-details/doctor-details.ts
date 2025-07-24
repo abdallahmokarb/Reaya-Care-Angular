1bcd9c3b46d1aab07b86af5c53876d52c85a9c70
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-24 22:28:47 +0300

    updated route link in all-doctors to route me to doctor-details.

---
+++ src/app/pages/doctor-details/doctor-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-details',
+  imports: [],
+  templateUrl: './doctor-details.html',
+  styleUrl: './doctor-details.css'
+})
+export class DoctorDetails {
+
+}
