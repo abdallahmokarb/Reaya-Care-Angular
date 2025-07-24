df77c93b6dca7cecce9c44c3fc2510440f8d8de0
Abdullah Moqreb <abdallah.mokarb@gmail.com>
2025-07-24 04:07:25 +0300

    completed main home page UI components by am

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ImageCarousel } from '../../components/homepage-compoents/image-carousel/image-carousel';
 import { HeroSearch } from '../../components/homepage-compoents/hero-search/hero-search';
+import { StickContent } from '../../components/homepage-compoents/stick-content/stick-content';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
-  imports: [CommonModule, HeroSearch, ImageCarousel],
+  imports: [CommonModule, HeroSearch, ImageCarousel, StickContent],
 })
 export class Home {}
