fe6ccbbdda80ca9b59675c6904b90da7f86682ed
sayedAli <said.ali199859@gmail.com>
2025-07-25 12:00:06 +0300

    added registration service , and environment, iregister

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -4,6 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IRegister } from '../../../models/iregister';
+import { RegistrationService } from '../../../shared/services/registration-service';
 
 @Component({
   selector: 'app-register',
@@ -14,13 +16,14 @@ import { FormsModule } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm: FormGroup;
+  private registrationService = inject(RegistrationService);
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router) {
     this.registerForm = this.fb.group({
       fullName: ['', Validators.required],
-      username: ['', Validators.required],
+      userName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
       isDoctor: [false], // false = patient, true = doctor
     });
@@ -32,10 +35,18 @@ export class RegisterComponent {
 
   onSubmit() {
     if (this.registerForm.valid) {
-      // Handle registration logic here
-      console.log(this.registerForm.value);
-    } else {
-      this.registerForm.markAllAsTouched();
+      const userData: IRegister = this.registerForm.value;
+      this.registrationService.registerUser(userData).subscribe({
+        next: (response: string) => {
+          console.log(response); // response is your string
+          this.router.navigate(['/']); // route to home
+        },
+        error: (error: any) => {
+          console.error('Registration failed', error);
+        }
+      });
+      } else {
+        this.registerForm.markAllAsTouched();
+      }
     }
-  }
 }
