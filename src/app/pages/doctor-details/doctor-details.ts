7527cb4c3bb8544ca59f8b3f65fd4db1abee1871
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-26 21:32:37 +0300

    updated doctor-details to view data dynamically.

--- src/app/pages/doctor-details/doctor-details.ts
+++ src/app/pages/doctor-details/doctor-details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Idoctordetails } from '../../models/idoctordetails';
+import { DoctorService } from '../../shared/services/doctor-service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-doctor-details',
@@ -6,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './doctor-details.html',
   styleUrl: './doctor-details.css'
 })
-export class DoctorDetails {
+export class DoctorDetails implements OnInit {
+  doctorId!: number;
+  doctor?: Idoctordetails;
+  isLoading = true;
+
+  route = inject(ActivatedRoute);
+  doctorService = inject(DoctorService);
+
+  ngOnInit(): void {
+    this.doctorId = Number(this.route.snapshot.paramMap.get('id'));
+    this.doctorService.getDoctorById(this.doctorId).subscribe({
+      next: (data) => {
+        this.doctor = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error fetching doctor details:', err);
+        this.isLoading = false;
+      }
+    });
+  }
 
 }
