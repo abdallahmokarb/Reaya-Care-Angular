43a97dfc8ffadfef5afe80e748722a1d70084a45
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-26 21:25:53 +0300

    added doctor service with two functions, get-all, get-by-id.

---
+++ src/app/shared/services/doctor-service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Idoctor } from '../../models/idoctor';
+import { Idoctordetails } from '../../models/idoctordetails';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorService {
+
+  private baseUrl = 'http://localhost:5216/api/Doctor'; // 👈 Update this to your real API URL
+
+  constructor(private http: HttpClient) {}
+
+  getAllDoctors(): Observable<Idoctor[]> {
+    return this.http.get<Idoctor[]>(this.baseUrl);
+  }
+
+  getDoctorById(id: number): Observable<Idoctordetails> {
+    return this.http.get<Idoctordetails>(`${this.baseUrl}/${id}`);
+  }
+}
