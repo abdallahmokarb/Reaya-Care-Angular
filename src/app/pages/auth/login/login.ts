966348c6084c85beb665462c0eb00232d9e9f06f
sayedAli <said.ali199859@gmail.com>
2025-07-27 14:48:50 +0300

    Merge branch 'main' into sayed

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../shared/services/auth-service';
 
 @Component({
   selector: 'app-login',
@@ -16,13 +17,29 @@ export class LoginComponent {
     password: '',
     remember: false
   };
+
+  constructor(private authService: AuthService) {}
+
   onSubmit(form: any) {
     if (form.valid) {
+      const credentials = {
+        email: this.model.email,
+        password: this.model.password
+      };
+
       console.log('Form Data:', this.model);
 
-      // this.authService.login(this.model).subscribe(...)
+      this.authService.login(credentials, this.model.remember).subscribe(
+        response => {
+          console.log('Login successful:', response);
+        },
+        error => {
+          console.error('Login failed:', error);
+        }
+      );
+
     } else {
-      console.warn('Form is invalid');
+      console.warn('Data is invalid');
     }
   }
 }
