13fccc517af528c24dc8ba15e5f069f9fabd53c8
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-27 05:17:14 +0300

    updated doctor details to view available time slots.

--- src/app/pages/doctor-details/doctor-details.ts
+++ src/app/pages/doctor-details/doctor-details.ts
@@ -1,23 +1,41 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Pipe } from '@angular/core';
 import { Idoctordetails } from '../../models/idoctordetails';
 import { DoctorService } from '../../shared/services/doctor-service';
 import { ActivatedRoute } from '@angular/router';
+import { TimeslotService } from '../../shared/services/timeslot-service';
+import { Itimeslot } from '../../models/itimeslot';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-doctor-details',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './doctor-details.html',
   styleUrl: './doctor-details.css'
 })
 export class DoctorDetails implements OnInit {
+  today: string;
   doctorId!: number;
   doctor?: Idoctordetails;
+  availableTimeSlots: Itimeslot[] = [];
   isLoading = true;
+
+  selectedDate: string = new Date().toISOString().split('T')[0]; // YYYY-MM-DD
+
+  constructor() {
+    // Initialize today's date in YYYY-MM-DD format for the date input
+    const todayDate = new Date();
+    this.today = todayDate.toISOString().split('T')[0];
+  }
 
   route = inject(ActivatedRoute);
   doctorService = inject(DoctorService);
+  timeslotService = inject(TimeslotService);
 
   ngOnInit(): void {
+
+
     this.doctorId = Number(this.route.snapshot.paramMap.get('id'));
     this.doctorService.getDoctorById(this.doctorId).subscribe({
       next: (data) => {
@@ -29,6 +47,37 @@ export class DoctorDetails implements OnInit {
         this.isLoading = false;
       }
     });
+
+     this.getTimeSlots();
+
   }
+
+
+  onDateChange() {
+    this.getTimeSlots();
+  }
+
+  getTimeSlots() {
+    const date = new Date(this.selectedDate);
+    this.getAvailableTimeSlotsForDoctor(this.doctorId, date).subscribe({
+      next: (slots) => (this.availableTimeSlots = slots),
+      error: (err) => console.error(err),
+    });
+  }
+
+
+  getAvailableTimeSlotsForDoctor(doctorId: number, date: Date): Observable<Itimeslot[]> {
+    return this.timeslotService.getAvailableTimeSlotsForDoctor(doctorId, date);
+  }
+
+  formatArabicTime(date: Date | string): string {
+  const time = new Date(date); // ensure it's a Date object
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const arabicPeriod = hours < 12 ? 'صباحًا' : 'مساءً';
+  const hour12 = hours % 12 || 12;
+
+  return `${hour12}:${minutes.toString().padStart(2, '0')} ${arabicPeriod}`;
+}
 
 }
