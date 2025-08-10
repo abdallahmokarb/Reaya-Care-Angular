7bc6d48e77d3fec6ab7d2cd5affede64d2298166
Abdullah Moqreb <abdallah.mokarb@gmail.com>
2025-08-10 17:45:40 +0300

    general improvements and bug fixes in admin and patient dashboards ,now refund status appear in main tables by am

--- src/app/shared/services/system-info.ts
+++ src/app/shared/services/system-info.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class SystemInfoService {
-  private apiUrl = 'http://localhost:5216/api/SystemInfo';
+  private apiUrl = 'http://localhost:5216/api/SystemInfo/CalcBalance';
 
   constructor(private http: HttpClient) {}
 
   getSystemInfo() {
-    return this.http.get(this.apiUrl, { responseType: 'text' }); // responsetype as text
+    return this.http.put(this.apiUrl, { responseType: 'text' }); // responsetype as text
   }
 }
